6895cf665067e8ace85f0efa69b683bfe1c28538
BuildTools <unconfigured@null.spigotmc.org>
2019-09-06 18:46:30 +0800

    Make global variables static...

--- src/globals.c
+++ src/globals.c
@@ -1,4 +1,4 @@
-struct message currentMessage;
-struct coords currentCoords;
-struct settings settings;
-struct device_state deviceState;
+static struct message currentMessage;
+static struct coords currentCoords;
+static struct settings settings;
+static struct device_state deviceState;
